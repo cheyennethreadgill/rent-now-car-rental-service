978b1ff95e59f1873434c180735ab826c156525e
cethread <threadgillcheyenne@gmail.com>
2024-01-22 11:31:50 -0500

    revisions

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-import './index.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import React from 'react';
-import CarSearch from './components/Car/Car Search/CarSearch';
-import Contact from './components/Contact/Contact';
-import Car from './components/Car/Car';
-import Home from './components/Home/Home';
+// import './index.css';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import React from 'react';
+// import CarSearch from './components/Car/Car Search/CarSearch';
+// import Contact from './components/Contact/Contact';
+// import Car from './components/Car/Car';
+// import Home from './components/Home/Home';
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/Products/:productId" element={<Car />} />
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/Products/:productId" element={<Car />} />
 
-        <Route path="/CarSearch/Products/:productId" element={<Car />} />
+//         <Route path="/CarSearch/Products/:productId" element={<Car />} />
 
-        <Route path="/" element={<Home />} />
+//         <Route path="/" element={<Home />} />
 
-        <Route path="/CarSearch" element={<CarSearch />} />
+//         <Route path="/CarSearch" element={<CarSearch />} />
 
-        <Route path="/Contact" element={<Contact />} />
+//         <Route path="/Contact" element={<Contact />} />
 
-        <Route path="/Car" element={<Car />} />
-      </Routes>
-    </BrowserRouter>
-  );
-};
+//         <Route path="/Car" element={<Car />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// };
 
-export default App;
+// export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,47 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+import Home from './components/Home/Home';
+import Car from './components/Car/Car';
+import CarSearch from './components/Car/Car Search/CarSearch';
+import Contact from './components/Contact/Contact';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/Products/:productId',
+    element: <Car />,
+  },
+  {
+    path: '/CarSearch/Products/:productId',
+    element: <Car />,
+  },
+  {
+    path: '/CarSearch',
+    element: <CarSearch />,
+  },
+  {
+    path: '/Contact',
+    element: <Contact />,
+  },
+  {
+    path: '/Car',
+    element: <Car />,
+  },
+]);
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
